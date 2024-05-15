91a490418d714f7e79c4db39a83ec08d5aeb7e6c
hoo01 <497458301@qq.com>
2024-05-15 22:38:01 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,36 +1,23 @@
 import gradio as gr
 import os
-import subprocess
 import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer, AutoModel
+from openxlab.model import download
 
 base_path = './hoo01_robot/'
-
-# Clone the repository
-clone_command = f'git clone https://code.openxlab.org.cn/hoo01/hoo01_robot.git {base_path}'
-clone_process = subprocess.Popen(clone_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-out, err = clone_process.communicate()
-print(out.decode('utf-8'))
-print(err.decode('utf-8'))
-
-# Pull from the repository
-pull_command = f'cd {base_path} && git lfs pull'
-pull_process = subprocess.Popen(pull_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-out, err = pull_process.communicate()
-print(out.decode('utf-8'))
-print(err.decode('utf-8'))
+os.system(f'git clone https://code.openxlab.org.cn/hoo01/hoo01_robot.git {base_path}')
+os.system(f'cd {base_path} && git lfs pull')
 
 tokenizer = AutoTokenizer.from_pretrained(base_path,trust_remote_code=True)
-model = AutoModelForCausalLM.from_pretrained(base_path,trust_remote_code=True, torch_dtype=torch.float16).cuda()
-model = model.eval()
+model = AutoModelForCausalLM.from_pretrained(base_path,trust_remote_code=True)
 
 def chat(message,history):
     for response,history in model.stream_chat(tokenizer,message,history,max_length=2048,top_p=0.7,temperature=1):
         yield response
 
 gr.ChatInterface(chat,
-                 title="HuZhenghui-Robot",
+                 title="hoo01_robot",
                 description="""
-我是hoo01的个人小助手.  
+我是hoo01的robot.  
                  """,
                  ).queue(1).launch()
